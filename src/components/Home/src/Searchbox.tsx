bd7641697a8337f185a205f7cd31fff4258fa3d1
Brandon Ly <wowbob396@gmail.com>
2020-01-05 11:08:35 -0600

    added map with autocomplete

--- src/components/Home/src/Searchbox.tsx
+++ src/components/Home/src/Searchbox.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState, ChangeEvent, SyntheticEvent, useMemo } from "react";
-import { TextField } from "@material-ui/core";
+import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import parse from 'autosuggest-highlight/parse';
 import throttle from 'lodash/throttle';
+
 
 interface ISearchBox {
   map: any;
@@ -22,13 +28,46 @@ interface PlaceType {
   };
 }
 
+function loadScript(src: string, position: HTMLElement | null, id: string) {
+  if (!position) {
+    return;
+  }
+
+  const script = document.createElement('script');
+  script.setAttribute('async', '');
+  script.setAttribute('id', id);
+  script.src = src;
+  position.appendChild(script);
+}
+
 const autocompleteService = { current: null };
 
+const useStyles = makeStyles(theme => ({
+  icon: {
+    color: theme.palette.text.secondary,
+    marginRight: theme.spacing(2),
+  },
+}));
+
 export const SearchBox: React.FC<ISearchBox> = props => {
+  const classes = useStyles();
+
   const [searchInput, setSearchInput] = useState("");
   const [options, setOptions] = useState<PlaceType[]>([]);
+  const loaded = React.useRef(false);
 
-  const { mapsApi } = props; 
+  if (typeof window !== 'undefined' && !loaded.current) {
+    if (!document.querySelector('#google-maps')) {
+      loadScript(
+        'https://maps.googleapis.com/maps/api/js?key=' +process.env.REACT_APP_GOOGLE_MAPS_API_KEY! +'&libraries=places',
+        document.querySelector('head'),
+        'google-maps',
+      );
+    }
+
+    loaded.current = true;
+  }
+  const { mapsApi, map } = props; 
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchInput(event.target.value);
@@ -44,8 +83,9 @@ export const SearchBox: React.FC<ISearchBox> = props => {
   useEffect(() => {
     let active = true;
 
-    if (!autocompleteService.current && mapsApi) {
-      autocompleteService.current = mapsApi.places.AutoCompleteService();
+    if (!autocompleteService.current && (window as any).google) {
+      console.log(mapsApi.places);
+      autocompleteService.current = new (window as any).google.maps.places.AutocompleteService();
     }
 
     if (!autocompleteService.current) return undefined;
@@ -67,9 +107,50 @@ export const SearchBox: React.FC<ISearchBox> = props => {
   }, [searchInput, fetch]);
 
   return(
-    <TextField
-      id="place-search"
-      name="place-search"
-    />  
+    <Autocomplete
+      id="google-map-demo"
+      style={{ width: 300 }}
+      getOptionLabel={option => (typeof option === 'string' ? option : option.description)}
+      filterOptions={x => x}
+      options={options}
+      autoComplete
+      includeInputInList
+      freeSolo
+      disableOpenOnFocus
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="Add a location"
+          variant="outlined"
+          fullWidth
+          onChange={handleChange}
+        />
+      )}
+      renderOption={option => {
+        const matches = option.structured_formatting.main_text_matched_substrings;
+        const parts = parse(
+          option.structured_formatting.main_text,
+          matches.map((match: any) => [match.offset, match.offset + match.length]),
+        );
+
+        return (
+          <Grid container alignItems="center">
+            <Grid item>
+              <LocationOnIcon className={classes.icon} />
+            </Grid>
+            <Grid item xs>
+              {parts.map((part: any, index: any) => (
+                <span key={index} style={{ fontWeight: part.highlight ? 700 : 400 }}>
+                  {part.text}
+                </span>
+              ))}
+              <Typography variant="body2" color="textSecondary">
+                {option.structured_formatting.secondary_text}
+              </Typography>
+            </Grid>
+          </Grid>
+        );
+      }}
+    />
   )
 }
