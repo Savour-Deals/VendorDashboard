fa0c72ddd0fdfd260321c6d3da5a9952efa7fc95
Brandon Ly <wowbob396@gmail.com>
2020-09-07 16:17:03 -0500

    added functionality to edit deals

--- src/components/Home/src/VendorModal.tsx
+++ src/components/Home/src/VendorModal.tsx
@@ -1,5 +1,7 @@
-import { Card, CardContent, CardHeader, createStyles, Fade, Grid, IconButton, makeStyles, Modal, Theme, TextField } from "@material-ui/core";
+import { Card, CardContent, CardHeader, createStyles, Fade, Grid, IconButton, makeStyles, Modal, Theme, TextField, Button } from "@material-ui/core";
 import EditIcon from "@material-ui/icons/Edit";
+import CancelIcon from "@material-ui/icons/Cancel";
+
 import React from 'react';
 
 interface IVendorModal {
@@ -14,12 +16,21 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     display: "inline-block",
     alignContent: "center",
     alignItems: "center",
-    width: "70%",
+    width: "100%",
     height: "70%",
   },
   cardContent: {
     alignItems: "center",
+    width: "80%"
   },
+  button: {
+    backgroundColor: "#49ABAA",
+    color: "white",
+    margin: theme.spacing(2),
+    fontSize: '8px',
+    height: '3em'
+  },
+
 }));
 
 const VendorModal: React.FC<IVendorModal> = props => {
@@ -28,9 +39,13 @@ const VendorModal: React.FC<IVendorModal> = props => {
 
   const styles = useStyles();
 
+  function runDeal(): void {
+    console.log(runDeal)
+  }
+
   return (
-      <Grid key={vendor.placeId} item xs={4} >
-      <Card>
+      <Grid key={vendor.placeId} item xs={6}>
+      <Card className={styles.cardContent}>
         <CardHeader
           title={vendor.vendorName}
           subheader={vendor.primaryAddress}
@@ -44,6 +59,7 @@ const VendorModal: React.FC<IVendorModal> = props => {
           <Grid container spacing={3}> 
             <Grid item xs={4}>
               Onboard Deal: {vendor.onboardDeal}
+           
             </Grid>
             <Grid item xs={4}>
               Single Click Deal: {vendor.singleClickDeal}
@@ -51,16 +67,40 @@ const VendorModal: React.FC<IVendorModal> = props => {
             <Grid item xs={4}>
               Double Click Deal: {vendor.doubleClickDeal}
             </Grid>
-            <Grid item container xs={4}>
+            <Grid item xs={4}>
+            <Button 
+                    variant="contained"   
+                    className={styles.button} 
+                    onClick={runDeal}>
+                      Run Onboard Deal
+                  </Button> 
+            </Grid>
+            <Grid item xs={4}>
+            <Button 
+                    variant="contained"   
+                    className={styles.button} 
+                    onClick={runDeal}>
+                      Run Single-click Deal
+                  </Button> 
+            </Grid>
+            <Grid item xs={4}>
+            <Button 
+                    variant="contained"   
+                    className={styles.button} 
+                    onClick={runDeal}>
+                      Run Double-click Deal
+                  </Button> 
+            </Grid>
+            {/* <Grid item container xs={4}>
               Current Subscribers:
               { vendor.subscribers ? Object.keys(vendor.subscribers).map((key: string, index: number) =>{
                 return (
                   <Grid item xs={4} key={index}>
-                    {vendor.subscribers!}
+                    {(vendor.subscribers) ? vendor.subscribers! : null}
                   </Grid>
                 )
               }) : null}
-            </Grid>
+            </Grid> */}
           </Grid>
           <Modal 
             open={vendorState[vendor.placeId]}
@@ -68,18 +108,34 @@ const VendorModal: React.FC<IVendorModal> = props => {
           >
             <Fade in={vendorState[vendor.placeId]}>
               <Card  className={styles.card}>
+                <CardHeader
+                    title={vendor.vendorName}
+                    subheader={vendor.primaryAddress}
+                    action={
+                      <CancelIcon onClick={() => toggleVendorModal(vendor.placeId, false)}>
+                        <EditIcon/>
+                      </CancelIcon>
+                    }
+                  />
                 <CardContent className={styles.cardContent}>
                   <Grid container spacing={3}>
                     <Grid item xs={4}>
-                      Onboard Deal: {vendor.onboardDeal}
-
-                      <TextField></TextField>
+                      <TextField 
+                        label="Onboard Deal"
+                        value={vendor.onboardDeal}
+                      />
                     </Grid>
                     <Grid item xs={4}>
-                      Single Click Deal: {vendor.singleClickDeal}
+                      <TextField 
+                          label="Single-click Deal"
+                          value={vendor.singleClickDeal}
+                        />                    
                     </Grid>
                     <Grid item xs={4}>
-                      Double Click Deal: {vendor.doubleClickDeal}
+                      <TextField 
+                          label="Onboard Deal"
+                          value={vendor.onboardDeal}
+                        />                    
                     </Grid>
                   </Grid>
                 </CardContent>
